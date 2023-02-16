75b7d517b96ba2b920466ed094be25d98e0054ac
Willwartz <rschkitzwartz@gmail.com>
2023-02-16 01:08:01 -0800

    puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - program entry point
+ *
+ * printf - prints string to standard output
+ *
+ * Return: always set to 0 indicating end of program
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a pirce of art,");
+	return (0);
+}
