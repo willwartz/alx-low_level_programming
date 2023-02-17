bae1117a53700d0cf8950bdf5dcbe5b8712f2001
willwartz <rschkitzwartz@gmail.com>
2023-02-17 15:23:32 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry Point
+ *
+ * printf - Print to output
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+	{
+			putchar(i);
+			if (i != 10)
+			{
+				putchar(',')
+				putchar(' ')
+			}
+	}
+	putchar('\n');
+	return (0);
+}
