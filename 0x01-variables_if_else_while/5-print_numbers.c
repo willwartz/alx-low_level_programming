70c5e5531e868ab45c3490e7055cad20d58e46dc
Willwartz <rschkitzwartz@gmail.com>
2023-02-17 03:26:14 -0800

    Varibles & Loops

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry Point
+ *
+ * printf - Print to output
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i = 0 ;
+
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i);
+	return (0);
+}
