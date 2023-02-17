d76e854576bbdbc3c1b0591decc5f71fa0c9b422
willwartz <rschkitzwartz@gmail.com>
2023-02-17 20:35:33 +0300

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry Point
+ *
+ * printf - Print to output
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i, a, j, k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (a = 48; a < 58 ; a++)
+		{
+			for (j = 48; j < 58; j++)
+			{
+				for (k = 48; k < 58; k++)
+				{
+					if (k > a && i<= j)
+					{
+						putchar(i);
+						putchar(a);
+						putchar(' ');
+						putchar(j);
+						putchar(k);
+						// putchar(',');
+						// putchar(' '); 
+						if (i == 57 && a == 56)
+						{
+							break;
+						}
+						else {
+							putchar(',');
+							putchar(' ');
+						}
+						}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
