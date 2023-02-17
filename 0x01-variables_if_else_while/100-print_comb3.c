d4d27b88359042ada5f8a0f69cb3276a08d42658
willwartz <rschkitzwartz@gmail.com>
2023-02-17 16:41:19 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry Point
+ *
+ * printf - Print to output
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	char i, a;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (a = 48; a < 58 ; a++)
+		{
+			if (i < a)
+			{
+				putchar(i);
+				putchar(a);
+				if (i < 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			} 
+		}
+	}
+	putchar('\n');
+	return (0);
+}
