45a9e777b33e6b5c3d0c181b602977a2aded375a
willwartz <rschkitzwartz@gmail.com>
2023-02-17 15:27:38 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,10 +13,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
 			putchar(i);
-			if (i != 10)
+			if (i != 57)
 			{
 				putchar(',');
 				putchar(' ');
