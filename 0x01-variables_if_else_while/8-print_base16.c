7b5d964d5819fc995a1f4cd98472266844ff05bd
willwartz <rschkitzwartz@gmail.com>
2023-02-17 15:07:33 +0300

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry Point
+ *
+ * printf - Print to output
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 97; i < 103; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
