1b5b6f3941df60dcb346e9204baebcfcf303fab8
willwartz <rschkitzwartz@gmail.com>
2023-02-17 14:52:40 +0300

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry Point
+ *
+ * printf - Print to output
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
