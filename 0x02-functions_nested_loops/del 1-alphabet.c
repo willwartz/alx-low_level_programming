4933365f049e1b25ad7b3db1e1e41739287d7fbe
willwartz <rschkitzwartz@gmail.com>
2023-02-21 20:26:04 +0300

    Fixing Betty

---
+++ 0x02-functions_nested_loops/del 1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ *
+ * _putchar - Print to output
+ *
+ * Return: Always 0
+*/
+
+void print_alphabet(void)
+{
+    int i;
+    
+    for (i = 97; i <= 122; i++)
+        {
+            _putchar(i);
+        }
+        _putchar('\n');
+}
