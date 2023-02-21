295dbfb6f6b33c78a62381e439079ea9912ac396
willwartz <rschkitzwartz@gmail.com>
2023-02-21 09:48:54 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ *
+ * _putchar - Print to output
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+    int str[] = {20, 21};
+
+    _putchar(str[1]);
+    return (0);
+}
+
