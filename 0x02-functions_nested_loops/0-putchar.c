7e334c67091f687b365e8d33728a2c9c92c699df
Willwartz <rschkitzwartz@gmail.com>
2023-02-21 09:02:16 -0800

    Fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,6 +12,7 @@ int main(void)
 	int myStr[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int syz, cnt;
 	syz = sizeof(myStr) / sizeof(int);
+
 	for (cnt = 0; cnt < syz; cnt++)
 	{
 		_putchar(myStr[cnt]);
