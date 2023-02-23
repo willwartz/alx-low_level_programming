7637e8fcff8aa9fe4950c4ebf9850b1a91dd500a
willwartz <rschkitzwartz@gmail.com>
2023-02-23 14:46:54 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,11 +4,11 @@
  * _mul - multiplies two integers
  * @a: input number.
  * @b: input number.
- * 
+ *
  * Return: multiplication
  */
 
 int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
