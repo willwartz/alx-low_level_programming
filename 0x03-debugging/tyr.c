e897ce414d140f81e0f7c4e8f013c31811a938b3
willwartz <rschkitzwartz@gmail.com>
2023-02-23 14:39:23 +0300

    Update tyr.c

--- 0x03-debugging/tyr.c
+++ 0x03-debugging/tyr.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+/**
+ * main - prints largest prime factor.
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-	printf("Good Stuff");
-	return (0);
+	// long long int a = 612852475143;
+	// long long int i = 2;
+
+	// while (i <= a)
+	// {
+	// 	if (a % i == 0)
+	// 	{
+	// 		printf("%d ", i);
+	// 		a /= i;
+	// 	}
+	// 	else
+	// 		i++;
+	// }
+	// printf("\n");
+	// return (0);
+	// putchar((697 / 100) + 48);
+	// putchar(697 % 10);
+	int m = -49;
+	putchar(m * -1);
+
 }
