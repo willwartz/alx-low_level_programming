56728645f4ac261a611df3b5cba20939afcd1912
willwartz <rschkitzwartz@gmail.com>
2023-03-02 10:00:14 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,10 @@
 #include "main.h"
-
 /**
  * _strcat - concats the string pointed to by src,
- * replacing the terminating null byte, to the
- * buffer pointed to by dest.
  * @dest: destination.
  * @src: source.
  * Return: the pointer to dest.
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int count1 = 0, count2 = 0;
@@ -22,12 +18,10 @@ char *_strcat(char *dest, char *src)
 	{
 		*(dest + count1) = *(src + count2);
 		if (*(src + count2) == '\0')
-		{
 			break;
-		}
 		count1++;
 		count2++;
 	}
-	return(dest);
+	return (dest);
 }
 
