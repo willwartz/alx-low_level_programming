bd17403ab321652ec4264209a93467469732d0f4
willwartz <rschkitzwartz@gmail.com>
2023-03-02 13:06:01 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,14 +7,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-        int i = 0, df = 0;
+	int i = 0, df = 0;
 
-        while (df == 0)
-        {
-                if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-                        break;
-                df = *(s1 + i) - *(s2 + i);
-                i++;
-        }
-        return(df);
+	while (df == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		df = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+	return(df);
 }
