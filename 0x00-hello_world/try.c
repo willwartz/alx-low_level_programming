3dc038a123117fdf19f4c4c9ed38d1d7467379d6
Willwartz <rschkitzwartz@gmail.com>
2023-03-02 12:38:54 -0800

    final Update

---
+++ 0x00-hello_world/try.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - program entry point
+ *
+ * puts - prints string to standard output
+ *
+ * Return: always set to 0 indicating end of program
+ */
+
+int main(void)
+{
+	a = 0;
+	for (; a < b;)
+	{
+	    printf("%d\n", a++);
+	}
+	return (0);
+}
