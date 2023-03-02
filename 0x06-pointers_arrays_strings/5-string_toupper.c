80041435124cc05929d4dc8ca74778467c425098
willwartz <rschkitzwartz@gmail.com>
2023-03-02 22:53:17 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,13 +6,13 @@
  */
 char *string_toupper(char *s)
 {
-        int count = 0;
+	int count = 0;
 
-        while (*(s + count) != '\0')
-        {
-                if ((*(s + count) >= 97) && (*(s + count) <= 122))
-                        *(s + count) = *(s + count) - 32;
-                count++;
-        }
-        return (s);
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
+	return (s);
 }
