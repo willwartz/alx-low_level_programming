a3d5d795ad62f2ccde737853949b2e37f9bdb42d
willwartz <rschkitzwartz@gmail.com>
2023-03-06 19:02:24 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,5 +2,8 @@
 #define _MAIN_H_
 
 int _putchar(char);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
