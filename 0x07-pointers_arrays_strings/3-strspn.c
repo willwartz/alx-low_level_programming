7fc8c689845a7e95ef6a3a3f59eb62ad89ae3f29
willwartz <rschkitzwartz@gmail.com>
2023-03-06 19:55:37 +0300

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: the string.
+ * @accept: substring of interest.
+ * Return: the pointer to dest.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0, i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0, *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + i))
+			{
+				count++;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	return (count);
+
+}
