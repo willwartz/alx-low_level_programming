bd82aeea9a140fc7a93da2ee98005ddac85c2e65
willwartz <rschkitzwartz@gmail.com>
2023-03-08 21:54:57 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,9 +12,9 @@ int root_finder(int n, int i)
 
 	sq = (i * i);
 	if (i <= (n / 2) && sq != n)
-		return(root_finder(n, i + 1));
-	else if(sq == n)
-    		return (i);
+		return (root_finder(n, i + 1));
+	else if (sq == n)
+		return (i);
 	else
 		return (-1);
 }
@@ -28,10 +28,10 @@ int root_finder(int n, int i)
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-    		return (-1);
-    	if (n == 0)
-    		return (0);
-   	if (n == 1)
-    		return (1);
-   	return (root_finder(n , 1));
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (root_finder(n, 1));
 }
