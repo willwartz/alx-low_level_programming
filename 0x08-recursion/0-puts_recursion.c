1d095468afbd08fd7d3bd1e478dd5e5c2556932e
willwartz <rschkitzwartz@gmail.com>
2023-03-08 21:31:33 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,13 @@
  * @s: pointer to string.
  * Return: no return.
  */
- void _puts_recursion(char *s)
- {
- 	if (*s != '\0')
- 	{
- 		_putchar(*s);
- 		_puts_recursion(s + 1);
- 	}
- 	else
- 		_putchar('\n');
- }
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
