3623a8440677114e04c12fbf358df2f419b772f1
willwartz <rschkitzwartz@gmail.com>
2023-03-14 15:05:11 +0300

    Update 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars,initializes
+ * it with a specific char
+ * @c: character.
+ * @size: size.
+ * @i: iterator.
+ * @s: array.
+ * Return: pointer to array.
+ */
+char *create_array(unsigned int size, char c)
+{
+        char *s;
+        unsigned int i;
+
+        if (size == 0)
+                return (NULL);
+
+        s = malloc(sizeof(c) * size)
+
+        if (cr == NULL)
+                return (NULL);
+
+        for (i = 0; i < size; i++)
+                cr[i] = c;
+
+        return (s);
+}
