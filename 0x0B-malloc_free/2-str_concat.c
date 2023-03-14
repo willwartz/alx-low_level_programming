a226c379aaf071f7ec440e35464ec609ef231172
willwartz <rschkitzwartz@gmail.com>
2023-03-14 18:08:37 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
         for (i = 0; s1[i] != '\0'; i++)
                 ;
 
-        for (j = 0; s1[j] != '\0'; j++)
+        for (j = 0; s2[j] != '\0'; j++)
                 ;
 
         constr = malloc(sizeof(char) * (i + j + 1));
