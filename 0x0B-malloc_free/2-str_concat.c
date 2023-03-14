21e2cd059d7fe8d357306530b2ac119dd41fe19a
willwartz <rschkitzwartz@gmail.com>
2023-03-14 17:07:41 +0300

    Create 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string.
+ *
+ * Return: pointer of an array of chars
+ */
+char *str_concat(char *s1, char *s2)
+{
+        unsigned int i, j, k, l, cap;
+        char *constr;
+
+        if (s1 == NULL)
+                s1 = "";
+        if (s2 == NULL)
+                s2 = "";
+
+        for (i = 0; s1[i] != '\0'; i++)
+                ;
+
+        for (j = 0; s1[j] != '\0'; j++)
+                ;
+
+        constr = (char *)malloc(sizeof(char) * (i + j + 1));
+
+        if (constr == NULL)
+                return (NULL);
+        
+        for (k = 0; k <= i; k++)
+                constr[k] = s1[k];
+
+        cap = j;
+
+        for (j = 0; j <= cap; j++)
+                constr[k + 1] = s2[j];
+
+        return (constr);
+
+}
