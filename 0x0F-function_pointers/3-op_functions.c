fe661571c718cfc2aad51cb382a3b541ca51db7e
willwartz <rschkitzwartz@gmail.com>
2023-03-22 22:11:12 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -48,7 +48,7 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(100);
 	}
 	return (a / b);
 }
@@ -65,7 +65,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(100);
 	}
 	return (a % b);
 }
