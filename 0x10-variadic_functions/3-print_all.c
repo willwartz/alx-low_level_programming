bd529b189b060ed48ad6552fd42130b2aaf741a9
willwartz <rschkitzwartz@gmail.com>
2023-03-23 22:24:22 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,11 @@
 #include "variadic_functions.h"
 
+/**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: no return.
+ */
 void print_all(const char * const format, ...)
 {
 	va_list varlist;
