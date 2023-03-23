ac4a3a680d5216b1c0dc78f548fe318de407690d
willwartz <rschkitzwartz@gmail.com>
2023-03-23 22:23:32 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,12 +14,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	
+
 	va_start(varlist, n);
 
 	for (i = 0; i < n; i++)
 		sum += va_args(varlist, int);
-	
+
 	va_end(varlist);
 
 	return (0);
