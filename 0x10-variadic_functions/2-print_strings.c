5cc9880a46f60a69d2a9aacf17c7615a116ffcdb
willwartz <rschkitzwartz@gmail.com>
2023-03-23 22:23:43 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		else
 			printf("nil");
-		
+
 		if (separator && i < n - 1)
 			printf(("%s", separator));
 	}
