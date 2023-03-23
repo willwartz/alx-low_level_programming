03da094c9132361467103d04f6b22d1df8983fad
willwartz <rschkitzwartz@gmail.com>
2023-03-23 23:22:11 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,15 +9,15 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list varlist;
+	va_list valist;
 	unsigned int i;
 	char *str;
 
-	va_start(varlist, n);
+	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(varlist, char *);
+		str = va_arg(valist, char *);
 
 		if (str)
 			printf("%s", str);
@@ -28,7 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (separator)
 				printf("%s", separator);
 	}
-	
+
 	printf("\n");
-	va_end(varlist);
+	va_end(valist);
 }
