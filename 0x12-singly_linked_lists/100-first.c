1a1baa80235780068d507fcc462ae3a2c75e676d
willwartz <rschkitzwartz@gmail.com>
2023-03-30 18:48:25 +0300

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_before_main - function executed before main
+ * Return: no return.
+ */
+
+void print_before_main(void) __attribute__ ((constructor))
+{
+	printf("You're beat! and yet, you must allow");
+	printf(", \nI bore my house upon my back!\n");
+}
+	
