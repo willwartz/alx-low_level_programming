1469613f934649952fafd5ee65f87c272f344f59
Willwartz <rschkitzwartz@gmail.com>
2023-03-30 10:58:11 -0700

    fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: no return.
  */
 
-void print_before_main(void) __attribute__ ((constructor))
+void __attribute__ ((constructor)) print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow");
 	printf(", \nI bore my house upon my back!\n");
