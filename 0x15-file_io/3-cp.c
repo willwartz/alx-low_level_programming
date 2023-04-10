0213d71c91cf2563bc57950e586e573bc2bbb051
willwartz <rschkitzwartz@gmail.com>
2023-04-10 20:41:18 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,84 +2,72 @@
 #include <stdio.h>
 
 /**
- * copy_file - copies the content of a file to another file
- * @file_from: file to copy from
- * @file_to: file to copy to
- * Return: void
-*/
-void copy_file(const char *file_from, const char *file_to)
-{
-	int fd_from, fd_to, closeError;
-	char buff[1024];
-	ssize_t numChars, numWritten;
-
-	fd_from = open(file_from, O_RDONLY);
-	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-
-	if (fd_from == -1)
-		exit(98);
-
-	if (fd_to == -1)
-		exit(99);
-
-	numChars = 1024;
-	while (numChars == 1024)
-	{
-		numChars = read(fd_from, buff, 1024);
-		if (numChars == -1)
-		exit(98);
-
-		numWritten = write(fd_to, buff, numChars);
-		if (numWritten == -1)
-		exit(99);
-	}
-
-	closeError = close(fd_from);
-	if (closeError == -1)
-		exit(100);
-
-	closeError = close(fd_to);
-	if (closeError == -1)
-		exit(100);
-}
-
-/**
- * error_exit - prints error message and exits
- * @error: error code
- * @file: file name
+ * error_check - checks if files can be opened.
+ * @fd_from: file_from.
+ * @fd_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-void error_exit(int error, char *file)
+void error_check(int fd_from, int fd_to, char *argv[])
 {
-	switch (error)
+	if (fd_from == -1)
 	{
-		case 97:
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		break;
-		case 98:
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-		break;
-		case 99:
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		break;
-		case 100:
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-		break;
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	exit(error);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
 
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 on success, 97, 98, 99, 100 on failure
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
+	int fd_from, fd_to, closeError;
+	ssize_t numChars, numWritten;
+	char buff[1024];
+
 	if (argc != 3)
-		error_exit(97, NULL);
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
 
-	copy_file(argv[1], argv[2]);
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_check(fd_from, fd_to, argv);
 
+	numChars = 1024;
+	while (numChars == 1024)
+	{
+		numChars = read(fd_from, buff, 1024);
+		if (numChars == -1)
+			error_check(-1, 0, argv);
+		numWritten = write(fd_to, buff, numChars);
+		if (numWritten == -1)
+			error_check(0, -1, argv);
+	}
+
+	closeError = close(fd_from);
+	if (closeError == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	closeError = close(fd_to);
+	if (closeError == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
 	return (0);
 }
+
