9ebaed5427a5b07152d9e26cc1261685eb2ebefd
willwartz <rschkitzwartz@gmail.com>
2023-04-10 22:14:53 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,9 +19,6 @@ void error_check(int error, char *file)
 	case 99:
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		break;
-	case 100:
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
-		break;
 	}
 	exit(error);
 }
@@ -61,11 +58,16 @@ void copy_file(const char *file_from, const char *file_to)
 
 	closeError = close(fd_from);
 	if (closeError == -1)
-		error_check(100, fd_from);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
 
 	closeError = close(fd_to);
 	if (closeError == -1)
-		error_check(100, fd_to);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
 	}
 
 /**
