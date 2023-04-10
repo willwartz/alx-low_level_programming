be8c71b9f76776b160ece8aabcf0ef213fa1690d
willwartz <rschkitzwartz@gmail.com>
2023-04-10 22:18:38 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,58 +2,57 @@
 #include <stdio.h>
 
 /**
- * error_check - prints error message and exits
- * @error: error code
- * @file: file name
+ * error_check - checks if files can be opened.
+ * @fd_from: file_from.
+ * @fd_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-void error_check(int error, char *file)
+void error_check(int fd_from, int fd_to, char *argv[])
 {
-	switch (error)
+	if (fd_from == -1)
 	{
-	case 97:
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		break;
-	case 98:
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-		break;
-	case 99:
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		break;
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-	exit(error);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
 
 /**
- * copy_file - copies the content of a file to another file
- * @file_from: file to copy from
- * @file_to: file to copy to
- * Return: void
-*/
-void copy_file(const char *file_from, const char *file_to)
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, closeError;
-	char buff[1024];
 	ssize_t numChars, numWritten;
+	char buff[1024];
 
-	fd_from = open(file_from, O_RDONLY);
-	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
 
-	if (fd_from == -1)
-		error_check(98, file_from);
-
-	if (fd_to == -1)
-		error_check(99, file_to);
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_check(fd_from, fd_to, argv);
 
 	numChars = 1024;
 	while (numChars == 1024)
 	{
 		numChars = read(fd_from, buff, 1024);
 		if (numChars == -1)
-			error_check(98, file_from);
-
+			error_check(-1, 0, argv);
 		numWritten = write(fd_to, buff, numChars);
 		if (numWritten == -1)
-			error_check(99, file_to);
+			error_check(0, -1, argv);
 	}
 
 	closeError = close(fd_from);
@@ -66,23 +65,9 @@ void copy_file(const char *file_from, const char *file_to)
 	closeError = close(fd_to);
 	if (closeError == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-}
-
-/**
- * main - copies the content of a file to another file
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, 97, 98, 99, 100 on failure
- */
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-		error_check(97, NULL);
-
-	copy_file(argv[1], argv[2]);
-
 	return (0);
 }
+
